c0fafc7df53edc163bdaebdcd7348febdaa655a9
Covid19 Chennai <zoneschennai@gmail.com>
2021-05-12 00:47:26 +0530

    Initial Commit

---
+++ VaccineAvailability/VaccineAvailabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaccineAvailabilityApp.swift
+//  VaccineAvailability
+//
+//  Created by Sriram Narasimhan on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct VaccineAvailabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
